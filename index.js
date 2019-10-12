b249676f7df38524f204ffcfb7725632e970a298
antilou86 <antilou86@gmail.com>
2019-10-12 14:38:38 -0400

    all done.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require('express');
+const db = require('./data/db.js');
 
 // creates an express application using the express module
 const server = express();
-
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
@@ -28,6 +28,15 @@ server.get('/hobbits', (req, res) => {
         },
     ];
     res.status(200).json(hobbits);
+})
+
+//creates a new enpoint that returns a list of users stored on the server.
+server.get('/users', (req,res) => {
+    db.find(req.body)
+        .then(users => {
+            res.status(200).json(users)
+            })
+        .catch(res.status(500).json('oh, noez! theres a problem with the response'));
 })
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
